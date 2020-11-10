c689778944c5704c5ac71cb05c5af13b1a93a7f2
Tiago Silveira <tiago.alexandre.silveira@formandos.cinel.pt>
2020-11-10 18:28:31 +0000

    Nominate Gold and Notifications Update

--- ProjFinalCinelAirClient/Controllers/ClientAreaController.cs
+++ ProjFinalCinelAirClient/Controllers/ClientAreaController.cs
@@ -96,31 +96,20 @@ namespace ProjFinalCinelAirClient.Controllers
         public IActionResult Nominate_Gold()
         {
             var client = _clientRepository.GetClientByUserEmail(User.Identity.Name);
-            //var status = _statusRepository.GetClientStatusById(client.Id);
+            var clientHistoric_status = _historic_StatusRepository.GetClientHistoric_StatusById(client.Id);
 
-            var clientList = _clientRepository.GetAll().ToList();
-            var historic_status = _historic_StatusRepository.GetAll().ToList();
-            List<Client> NotgoldClientList = new List<Client>();
-
-            //get all clients with gold status
-            foreach (var cl in clientList)
-            {
-                foreach (var hs in historic_status)
-                {
-                    if (hs.ClientId == cl.Id)
-                    {
-                        if (hs.StatusId == 2 || hs.StatusId == 3)
-                        {
-                            NotgoldClientList.Add(cl);
-                        }
-                    }                
-                }
-            }
 
             var model = new NominateGoldViewModel
             {
-                ClientList = NotgoldClientList
+                ClientList = new List<Client>()
             };
+
+            if (clientHistoric_status.wasNominated == true)
+            {        
+                TempData["cant"] = "You already nominated a client or have been nominated by another client";
+
+                return View(model);
+            }
 
             return View(model);
         }
@@ -142,6 +131,12 @@ namespace ProjFinalCinelAirClient.Controllers
             {
                 ClientList = clients.ToList()
             };
+
+            if (clients.ToList().Count == 0)
+            {
+                TempData["message"] = "There are no clients with that Name or Number";
+            }
+            
             return View("Nominate_Gold", vmodel);
             
         }
@@ -149,16 +144,33 @@ namespace ProjFinalCinelAirClient.Controllers
 
         public async Task<IActionResult> Nominate_Gold_Confirm(int Id)
         {
-            //var client = await _clientRepository.GetByIdAsync(id);
+            var client = _clientRepository.GetClientByUserEmail(User.Identity.Name);
             var hstatus = _historic_StatusRepository.GetClientHistoric_StatusById(Id);
+            var selectedClient = await _clientRepository.GetByIdAsync(hstatus.Id);
 
             hstatus.StatusId = 1;
             hstatus.Start_Date = DateTime.Now;
             hstatus.End_Date = hstatus.Start_Date.AddYears(1);
             hstatus.wasNominated = true;
-            //TODO: fazer com que o user que nomeou não consiga nomear outra vez
-            //TODO: mandar notificação a este user que recebeu o status gold
 
+            var clientNotification = new Notification
+            {
+                ClientId = client.Id,
+                Date = DateTime.Now,
+                Subject = "Nominate Gold",
+                Message = $"You have nominated {selectedClient.FullName} with client number {selectedClient.Client_Number} for gold status "
+            };
+
+            var selectedClientNotification = new Notification
+            {
+                ClientId = hstatus.ClientId,
+                Date = DateTime.Now,
+                Subject = "Updated Status",
+                Message = $"You have been nominated for gold status by {client.FullName} with client number {client.Client_Number}." +
+                $"You can now enjoy all the benefits that come with gold status " +
+                $"From this point forward you may not nominate another client for gold status"               
+            };
+            //TODO: Mandar Email??
             await _historic_StatusRepository.UpdateAsync(hstatus);
 
             //TODO: apresentar mensagem nomenation sucessful
@@ -325,8 +337,25 @@ namespace ProjFinalCinelAirClient.Controllers
                 var selectedClientBonus = _mile_BonusRepository.GetAll().Where(u => u.ClientId == selectedClient.Id);
 
                 client.Miles_Bonus -= selectedAmount.MileQuantity;
+                client.AnnualMilesShopped += selectedAmount.MileQuantity;
                 //TODO: falar com dulce sobre Miles_Bonus
                 selectedClient.Miles_Bonus += selectedAmount.MileQuantity;
+
+                var clientMileBonus = new Mile_Bonus
+                {
+                    Miles_Number = selectedAmount.MileQuantity,
+                    //TODO: as milhas bonus quando transferidas ficam com que validade?
+                    available_Miles_Bonus = client.Miles_Bonus - selectedAmount.MileQuantity,
+                    ClientId = client.Id
+                };
+                var selectedClientMileBonus = new Mile_Bonus
+                {
+                    Miles_Number = selectedAmount.MileQuantity,
+                    //TODO: as milhas bonus quando transferidas ficam com que validade?
+                    available_Miles_Bonus = selectedClient.Miles_Bonus + selectedAmount.MileQuantity,
+                    ClientId = selectedClient.Id
+                };
+
                 var clientTransaction = new Transaction
                 {
                     ClientId = client.Id,
@@ -352,9 +381,11 @@ namespace ProjFinalCinelAirClient.Controllers
                 await _clientRepository.UpdateAsync(selectedClient);
                 await _transactionRepository.CreateAsync(clientTransaction);
                 await _transactionRepository.CreateAsync(selectedClientTransaction);
+                await _mile_BonusRepository.CreateAsync(clientMileBonus);
+                await _mile_BonusRepository.CreateAsync(selectedClientMileBonus);
 
                 TempData["t"] = "Transfer completed sucessfully!";
-                //TODO: redirecionar para a mesma página mandar mensagem de success
+               
                 return RedirectToAction("MileShopTransferMiles");               
             }
             else
@@ -366,6 +397,73 @@ namespace ProjFinalCinelAirClient.Controllers
         }
 
 
+        public IActionResult MileShopConvertMiles()
+        {
+            var client = _clientRepository.GetClientByUserEmail(User.Identity.Name);           
+            var shop = _buyMilesShopRepository.GetAll();
+            List<BuyMilesShop> sList = new List<BuyMilesShop>();
+
+            //lista contém só milhas que o cliente pode transferir com base nas milhas já compradas ou transferidas
+            foreach (var item in shop)
+            {
+                int aux = 20000 - client.AnnualMilesShopped;
+                if (item.MileQuantity <= aux)
+                {
+                    sList.Add(item);
+                }
+            }
+
+            var model = new ConvertMilesViewModel
+            {
+                Id = client.Id,
+                ShopList = sList
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MileShopConvertMiles_confirm(ConvertMilesViewModel model)
+        {
+            var client = await _clientRepository.GetByIdAsync(model.Id);
+            var selectedAmount = await _buyMilesShopRepository.GetByIdAsync(model.SelectedRadio);
+
+            client.Miles_Bonus -= selectedAmount.MileQuantity;
+            client.AnnualMilesShopped += selectedAmount.MileQuantity;
+
+            var clientMileBonus = new Mile_Bonus
+            {
+                Miles_Number = selectedAmount.MileQuantity,
+                //TODO: as milhas bonus quando transferidas ficam com que validade?
+                available_Miles_Bonus = client.Miles_Bonus - selectedAmount.MileQuantity,
+                ClientId = client.Id
+            };
+
+            var clientMileStatus = new Mile_Bonus
+            {
+                Miles_Number = selectedAmount.MileQuantity,
+                //TODO: as milhas bonus quando transferidas ficam com que validade?
+                available_Miles_Bonus = client.Miles_Bonus - selectedAmount.MileQuantity,
+                ClientId = client.Id
+            };
+
+
+            var clientTransaction = new Transaction
+            {
+                ClientId = client.Id,
+                Miles = selectedAmount.MileQuantity,
+                Date = DateTime.Now,
+                Movement_Type = "Transfer",
+                Description = $"You transfered {selectedAmount.MileQuantity}",
+                Balance_Miles_Bonus = client.Miles_Bonus - selectedAmount.MileQuantity,
+                Balance_Miles_Status = client.Miles_Status
+            };
+
+            //TODO: actualiza automáticamente subida de status?
+
+            return View();
+
+        }
 
     }
 }
---
+++ ProjFinalCinelAirClient/Models/ConvertMilesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ProjFinalCinelAir.CommonCore.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjFinalCinelAirClient.Models
+{
+    public class ConvertMilesViewModel : Client
+    {
+        public int SelectedRadio { set; get; }
+
+        public List<BuyMilesShop> ShopList { get; set; }
+
+    }
+}
